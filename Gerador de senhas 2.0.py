50e48597b1f26a750f65da3c70385cda3a1c3ea1
Arthur Albuquerque <110507968+ArtLeadAlb@users.noreply.github.com>
2023-09-04 21:14:41 -0300

    Add files via upload

---
+++ Gerador de senhas 2.0.py
@@ -0,0 +1,43 @@
+import random
+import string
+
+pontos = '!@#$%&*()_+=-[]{}/?'
+valores = string.ascii_letters + string.digits + pontos
+
+print('=' * 24)
+print(f'   PROGRAMA INICIADO')
+print('=' * 24)
+
+while True:
+    quantidade = int(input(' Quantos caracteres? '))
+    senha = ''
+    for i in range(quantidade):
+        senha += random.choice(valores)
+
+    print('-' * 90)
+    salvar = input(' Menu de Opções:\n [0] -> Gerar a senha e apagar quando o programa for fechado.\n [1] -> Salvar em um arquivo.\n Escolha : ')
+    if salvar == '1':
+        print('-' * 90)
+        arquivo_senha = open('Senha.txt','w')
+        assunto = input(' Para o que é a senha? ')
+        user = input(' Qual é o usuário? ')
+        arquivo_senha.write(f'Assunto: {assunto}\nUsuário: {user}\nSenha gerada: {senha}')
+        arquivo_senha.close()
+        print('-' * 90)
+        print(f' Senha salva em um arquivo "Senha.txt" no mesmo local que esse programa foi executado.')
+        print('-' * 90)
+
+    elif salvar == '0':
+        print('-' * 90)
+        print(f' Senha gerada: {senha}')
+        print('-' * 90)
+    else:
+        print(' Opção Inválida.')
+
+    sair = input(' Deseja sair? [S] / [N]: ').upper()
+    if sair == 'S':
+        break
+    elif sair == 'N':
+        continue
+    else:
+        print(' Opção Inválida.')
---
+++ Senha Wifi 2.0.py
@@ -0,0 +1,23 @@
+import subprocess
+
+informacoes = subprocess.check_output(["netsh", "wlan", "show", "profiles"], encoding='cp860')
+#print(informacoes)
+arquivo = open('Senhas do Wifi.txt','w')
+for i in informacoes.split('\n'):
+    if "Usuários: " in i:
+        p = i.find(':')
+        nome_wifi = i[p+2:]
+        informacoes = subprocess.check_output(["netsh", "wlan", "show", "profile", nome_wifi, "key", "=", "clear"], encoding='cp858')
+        #print(informacoes)
+        arquivo.write(informacoes)
+
+        for linha in informacoes.split('\n'):
+            if "Conteúdo da Chave" in linha:
+                # pegar a senha
+                pos = linha.find(":")
+                senha = linha[pos+2:]
+                #print(f'Wifi: {nome_wifi}\nSenha: {senha}\n')
+
+                arquivo.write(f'Wifi: {nome_wifi}\nSenha: {senha}\n')
+
+arquivo.close()
