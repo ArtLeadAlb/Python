05fd95a88164df23b5a67ab04923ad10349e47f5
Arthur Albuquerque <110507968+ArtLeadAlb@users.noreply.github.com>
2023-09-04 21:22:06 -0300

    Descobrir senhas Wifi 2.0.py

--- Senha Wifi 2.0.py
+++ Senha Wifi 2.0.py
@@ -1,14 +1,12 @@
 import subprocess
 
 informacoes = subprocess.check_output(["netsh", "wlan", "show", "profiles"], encoding='cp860')
-#print(informacoes)
 arquivo = open('Senhas do Wifi.txt','w')
 for i in informacoes.split('\n'):
     if "Usuários: " in i:
         p = i.find(':')
         nome_wifi = i[p+2:]
         informacoes = subprocess.check_output(["netsh", "wlan", "show", "profile", nome_wifi, "key", "=", "clear"], encoding='cp858')
-        #print(informacoes)
         arquivo.write(informacoes)
 
         for linha in informacoes.split('\n'):
@@ -16,7 +14,6 @@ for i in informacoes.split('\n'):
                 # pegar a senha
                 pos = linha.find(":")
                 senha = linha[pos+2:]
-                #print(f'Wifi: {nome_wifi}\nSenha: {senha}\n')
 
                 arquivo.write(f'Wifi: {nome_wifi}\nSenha: {senha}\n')
 
