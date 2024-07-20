59e88bb3c79229e420da632d1afe15ce3e6d32e3
Arthur Albuquerque <110507968+ArtLeadAlb@users.noreply.github.com>
2024-07-20 15:05:38 -0300

    Update Jogo da forca.py

--- Jogo da forca.py
+++ Jogo da forca.py
@@ -1,43 +1,48 @@
 """Famoso jogo da forca (ou palavra secreta)"""
 
 import os
-
-palavra = input('Digite a palavra para que o jogo comece:').lower()
-os.system('cls')
-print('')
-letras_acertadas = ''
-numero_tentativas = 0
-
 while True:
-    chute = input('Digite uma letra: ').lower()
-    numero_tentativas += 1
+        
+    palavra = input('Digite a palavra para que o jogo comece: ').lower()
+    dica = input('Digite uma dica: ')
+    os.system('cls')
+    print('')
+    letras_acertadas = ''
+    numero_tentativas = 0
+    print(f'Dica: {dica}.\n')
 
-    if len(chute) > 1:
-        print('Digite apenas uma letra, por favor.')
+    while True:
+
+        chute = input('Digite uma letra: ').lower()
+        numero_tentativas += 1
+
+        if len(chute) > 1:
+            print('Digite apenas uma letra, por favor.')
+            continue
+
+        if chute in palavra:
+            letras_acertadas += chute
+
+        palavra_formada = ''
+        for letra_secreta in palavra:
+            if letra_secreta in letras_acertadas:
+                palavra_formada += letra_secreta
+            else:
+                palavra_formada += '*'
+
+        print(f'Palavra formada: {palavra_formada}.')
+
+        if palavra_formada == palavra:
+            print('Parabéns! Você acertou a palavra!')
+            print(f'A palavra era {palavra}!')
+            print(f'Tentativas: {numero_tentativas}.')
+            break
+        
+    opcao = int(input('Deseja jogar novamente (1) ou sair (0)? '))
+    if opcao == 1:
         continue
-
-    if chute in palavra:
-        letras_acertadas += chute
-
-    palavra_formada = ''
-    for letra_secreta in palavra:
-        if letra_secreta in letras_acertadas:
-            palavra_formada += letra_secreta
-        else:
-            palavra_formada += '*'
-
-    print(f'Palavra formada: {palavra_formada}.')
-
-    if palavra_formada == palavra:
-        print('Parabéns! Você acertou a palavra!')
-        print(f'A palavra era {palavra}!')
-        print(f'Tentativas: {numero_tentativas}.')
-        sair=input("Para sair digite 's' e pressione Enter:")
-        if sair =='s':
-            exit()
-        else:
-            palavra = input('Digite a palavra para que o jogo comece:').lower()
-            os.system('cls')
-            print('')
-            letras_acertadas = ''
-            numero_tentativas = 0
+    elif opcao == 0:
+        break
+    else:
+        print('Opção inválida! Saindo...')
+        break
